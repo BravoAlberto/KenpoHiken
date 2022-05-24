91211c6bbf61f4c00f319505ae1a6c27c8ab92bf
BravoAlberto <proyectohiken@gmail.com>
2022-05-24 18:08:22 +0200

    Actualización documentación con datos tablas BBDD

---
+++ funciones/traeTutores.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once 'password.php';
+
+$usuario = $_POST['user'];
+$clave = Password::hash($_POST['password']);
+$con = new PDO("mysql:host=localhost; dbname=kenpohiken", 'administrador', 'AB492ga2');
+
+if (!empty($usuario) && (strlen($clave) >= 6 || strlen($clave) <= 8)) {
+    $sql = "SELECT * FROM usuario WHERE usuario = '".$usuario."'";
+    $query = $con->prepare($sql);
+    $query->execute();
+    $resultado = $query->fetch();
+    if ($resultado == null) {
+        echo ('1');
+    } else {
+        echo('0');
+    }
+}
+?>
